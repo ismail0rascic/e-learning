12073ba56030795d9e252145a566a6895d263c76
ismail0rascic <ismail0rascic@gmail.com>
2023-03-13 19:31:11 +0100

    main

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,13 @@
 import createProxyMiddleware from "http-proxy-middleware";
 
-module.exports = function (app) {
+/*module.exports = function (app) {
   app.use(
     "/",
     createProxyMiddleware({
-      target: "https://e-learning-978v.onrender.com/",
+      target: "https://e-learning-978v.onrender.com",
       changeOrigin: true,
       secure: false,
     })
   );
 };
+*/
